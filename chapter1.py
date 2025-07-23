d07e9aa0ecaeedd914ea501b43230a62b670f5a3
saji2000 <sajaddaneshmand79@gmail.com>
2025-07-22 23:40:01 -0400

    Comment

--- chapter1.py
+++ chapter1.py
@@ -1,5 +1,5 @@
 from transformers import pipeline
-
+# Masking
 unmasker = pipeline("fill-mask", model="bert-base-uncased")
 result = unmasker("This man works as a [MASK].")
 print([r["token_str"] for r in result])
