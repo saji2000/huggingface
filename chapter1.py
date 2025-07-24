73ca06dfd7dccca9d7146985cc056fe09b2aab19
saji2000 <sajaddaneshmand79@gmail.com>
2025-07-23 23:15:09 -0400

    Installing python and cuda again

--- chapter1.py
+++ chapter1.py
@@ -1,5 +1,5 @@
 from transformers import pipeline
-# Masking
+# Installing python and cuda again
 unmasker = pipeline("fill-mask", model="bert-base-uncased")
 result = unmasker("This man works as a [MASK].")
 print([r["token_str"] for r in result])
