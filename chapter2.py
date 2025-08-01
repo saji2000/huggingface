597c0490b70d13b2bcbc0b78ae613a0f584318d3
saji2000 <sajaddaneshmand79@gmail.com>
2025-07-31 23:36:10 -0400

    Downloaded bert tokenizer

--- chapter2.py
+++ chapter2.py
@@ -1,6 +1,15 @@
 from transformers import AutoTokenizer
+import os
 
+# Step 1: Load tokenizer from Hugging Face Hub (or use your custom model name)
 tokenizer = AutoTokenizer.from_pretrained("./bert-model")
 
+# Step 2: Save tokenizer locally to the model folder
+# tokenizer.from_pretrained("./bert-model")
+
+# Step 3: Print contents of the model directory
+print(os.listdir("./bert-model"))
+
+# Step 4: Test encoding a sentence
 encoded_input = tokenizer("Hello, I'm a single sentence!")
 print(encoded_input)
