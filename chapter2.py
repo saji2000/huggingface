903fd8b0f9b8257b1e1b3495135d8d41ac115e6d
saji2000 <sajaddaneshmand79@gmail.com>
2025-08-04 14:04:43 -0400

    handling sequences

--- chapter2.py
+++ chapter2.py
@@ -1,17 +1,18 @@
 import torch
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
-checkpoint = "distilbert-base-uncased-finetuned-sst-2-english"
+checkpoint = "./bert-model"
 tokenizer = AutoTokenizer.from_pretrained(checkpoint)
 model = AutoModelForSequenceClassification.from_pretrained(checkpoint)
 
-sequence = "I've been waiting for a HuggingFace course my whole life."
+sequence = ["I've been waiting for a HuggingFace course my whole life.", "Me too"]
 
-tokens = tokenizer.tokenize(sequence)
-ids = tokenizer.convert_tokens_to_ids(tokens)
+tokens = tokenizer(sequence, padding=True, truncation=True, return_tensors="pt")
+output = model(**tokens)
 
-input_ids = torch.tensor([ids])
-print("Input IDs:", input_ids)
+print(output)
+# input_ids = torch.tensor([ids])
+# print("Input IDs:", input_ids)
 
-output = model(input_ids)
-print("Logits:", output.logits)
+# output = model(input_ids)
+# print("Logits:", output.logits)
