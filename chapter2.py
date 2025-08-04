6dc78bbbdb3416898ba40b598b6e0b0e25762e23
saji2000 <sajaddaneshmand79@gmail.com>
2025-08-03 23:46:02 -0400

    sequences

--- chapter2.py
+++ chapter2.py
@@ -1,25 +1,17 @@
 import torch
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
-# Step 1: Load tokenizer from Hugging Face Hub (or use your custom model name)
-tokenizer = AutoTokenizer.from_pretrained("./bert-model")
-model = AutoModelForSequenceClassification.from_pretrained("./bert-model")
 
-
-# Step 2: Save tokenizer locally to the model folder
-# tokenizer.from_pretrained("./bert-model")
-
-# Step 3: Print contents of the model directory
-# print(os.listdir("./bert-model"))
-
-# Step 4: Test encoding a sentence
-# encoded_input = tokenizer("This is a preloaded model!")
-# print(encoded_input)
+checkpoint = "distilbert-base-uncased-finetuned-sst-2-english"
+tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+model = AutoModelForSequenceClassification.from_pretrained(checkpoint)
 
 sequence = "I've been waiting for a HuggingFace course my whole life."
 
 tokens = tokenizer.tokenize(sequence)
 ids = tokenizer.convert_tokens_to_ids(tokens)
-input_ids = torch.tensor(ids)
-# This line will fail.
-result = model(input_ids)
-print(result)
+
+input_ids = torch.tensor([ids])
+print("Input IDs:", input_ids)
+
+output = model(input_ids)
+print("Logits:", output.logits)
